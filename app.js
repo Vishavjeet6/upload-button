93af67d86e84203268accc99635441ba0534840f
Vishavjeet Singh <vishavjeetsinghthakur@gmail.com>
2021-08-08 11:09:42 +0530

    removing https

--- app.js
+++ app.js
@@ -9,19 +9,19 @@ const app = express();
 
 const config = require('./config');
 // const { HTTP_PORT, HTTPS_PORT, KEY_FILE, CERT_FILE, PASS_PHRASE } = config.env;
-const { HTTPS_PORT, KEY_FILE, CERT_FILE, PASS_PHRASE } = config.env;
+const { HTTP_PORT, KEY_FILE, CERT_FILE, PASS_PHRASE } = config.env;
 const { filesize , audiosize, uploadfilepath, uploadaudiopath} = config.file;
 
-var https = require('https');
-// var http = require('http');
+// var https = require('https');
+var http = require('http');
 var fs = require('fs');
 var path = require('path')
 
-var options = {
-    key: fs.readFileSync(KEY_FILE),
-    cert: fs.readFileSync(CERT_FILE),
-    passphrase: PASS_PHRASE
-  };
+// var options = {
+//     key: fs.readFileSync(KEY_FILE),
+//     cert: fs.readFileSync(CERT_FILE),
+//     passphrase: PASS_PHRASE
+//   };
 
 // enable css and js
 app.use(express.static(path.join(__dirname, '/public')));
@@ -171,10 +171,10 @@ app.post('/upload/audio', async (req, res) => {
 // const port = process.env.PORT || 3000;
 
 
-// http.createServer(app).listen(HTTP_PORT, () => 
-// console.log(`HTTP App is listening on port ${process.env.HTTP_PORT}`));
-https.createServer(options, app).listen(HTTPS_PORT, () => 
-console.log(`HTTPS App is listening on port ${process.env.HTTPS_PORT}`));
+http.createServer(app).listen(HTTP_PORT, () => 
+console.log(`HTTP App is listening on port ${process.env.HTTP_PORT}`));
+// https.createServer(options, app).listen(HTTPS_PORT, () => 
+// console.log(`HTTPS App is listening on port ${process.env.HTTPS_PORT}`));
 
 // app.listen(port, () => 
 //   console.log(`App is listening on port ${port}.`)
