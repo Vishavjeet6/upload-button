b7d5f1e914933e8d359a7a6557aad4fc4be1aff2
Vishavjeet Singh <vishavjeetsinghthakur@gmail.com>
2021-08-11 14:51:20 +0530

    removed body-parser, lodash

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const _ = require('lodash');
 
 const app = express();
 
@@ -26,8 +24,8 @@ app.use(fileUpload({
 
 //add other middleware
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
 
 app.get('/', function (req, res) {
